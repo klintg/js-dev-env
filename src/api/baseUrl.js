9852b8c9d05d6fde4997e0cd28e9b1a95658b650
klintg <theklint1@gmail.com>
2016-12-02 20:20:38 +0300

    production deploy

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:5001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:5001/' : 'https://mysterious-dawn-16770.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
