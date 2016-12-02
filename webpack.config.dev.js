6c6b8085661f2366fc4ebea91b9a62dd2b735a10
klintg <theklint1@gmail.com>
2016-12-02 15:08:53 +0300

    html-plugin details belonged in the production webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -18,18 +18,6 @@ export default {
     // Create HTML file that includes reference to bundled JS.
     new HtmlWebpackPlugin({
       template: 'src/index.html',
-       minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true
-      },
       inject: true
     })
   ],
